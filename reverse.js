24e86e37acea6df1f534bdab171b5d8b377ff4f2
DinaLysenko <dinafate@rambler.ru>
2023-06-06 00:29:05 +0500

    Add some test

--- reverse.js
+++ reverse.js
@@ -1,3 +1,10 @@
 function reverseWords(str) {
     return str.split(' ').map(word=>word.split('').reverse().join('')).join(' ');
   }
+
+  const reverseSeq = n => {
+    let arr =[];
+    for (let i = 1; i <= n; i++ ){
+     arr.push(i);
+    } return arr.reverse();
+  };
