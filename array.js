b26572ac1812c6cf9e6ffed4234fb1aadfb64339
DinaLysenko <dinafate@rambler.ru>
2023-06-05 13:13:08 +0500

    Add some test

--- array.js
+++ array.js
@@ -13,3 +13,44 @@ function arrayPlusArray(arr1, arr2) {
 
 // const myArray = [50, 60, 70];
 const myData = myArray[0];
+
+// const myArray = [18, 64, 99];
+myArray[0] = 45;
+
+const myArray = [
+   [1, 2, 3],
+   [4, 5, 6],
+   [7, 8, 9],
+   [[10, 11, 12], 13, 14],
+ ];
+//   const myData = myArray[2][1];
+
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+
+const myArray = [["John", 23], ["cat", 2]];
+const removedFromMyArray = myArray.pop();
+
+function oddOrEven(array) {
+  let arrySum=0;
+  for(let i=0;i<array.length;i++){
+    arrySum=arrySum+array[i];
+  }
+     if(arrySum===0){
+    return "even";
+  }else if(arrySum%2===0){
+    return "even";
+  }
+  else{
+    return "odd";
+  }
+}
+
+const myArray = [["John", 23], ["dog", 3]];
+// const removedFromMyArray = myArray.shift();
+
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35]);
+
+const myList = [["Banana", 5], ["Bread", 3], ["Milk", 4], ["Cheese", 18], ["Eggs", 10]];
